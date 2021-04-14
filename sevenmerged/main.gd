f7d71bba9d651d02adf726df7404661270d317c1
pringle newfangle <parker@cline.cc>
2021-04-13 17:12:12 -0700

    merged project again

---
+++ sevenmerged/main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+onready var camera = $camera
+onready var player = $player
+onready var testNode = $"test node"
+
+func ready():
+	pass
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		switchCamTo(testNode)
+	else:
+		switchCamTo(player)
+
+func switchCamTo(object):
+	camera.global_position.x = object.global_position.x
+	camera.global_position.y = object.global_position.y + -25
